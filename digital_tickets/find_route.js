b81a7667650f75b78d009304822d14dba16aed58
Timofey Gribanov <48593815+timagribanov@users.noreply.github.com>
2019-12-23 18:54:48 +0300

    Routes are being shown

--- digital_tickets/find_route.js
+++ digital_tickets/find_route.js
@@ -39,14 +39,43 @@ function getTripInfo() {
             break;
     }
 
+    let oneWayTrips = [];
     if (document.getElementById('one-way-trip').checked === true) {
         // Один билет
-        townFrom = '%' + townFrom +'%';
-        townTo = '%' + townTo + '%';
-        
-        let oneWayTripsID = [];
-        alasql('SELECT ID INTO ? FROM CSV("database/routes.csv", {headers:true, separator:";"}) WHERE TOWN_FROM = ? AND TOWN_TO = ?', [oneWayTripsID, townFrom, townTo]);
-        alert(oneWayTripsID);
+        alasql.promise('SELECT * INTO ? FROM CSV("database/routes.csv", {headers:true, separator:";"}) WHERE TOWN_FROM = ? AND TOWN_TO = ? AND DATE_DEP = ?', [oneWayTrips, townFrom, townTo, date])
+            .then(function(data) {
+                console.log(oneWayTrips);
+                for (let i = 0;i < oneWayTrips.length;i++) {
+                    document.getElementById('route-info').innerHTML += '<div class="route-block">\n' +
+                        '            <div class="row">\n' +
+                        '                <span class="town" id="route-town-from-' + i + '"></span>\n' +
+                        '                <span class="time" id="route-time-from-' + i + '"></span>\n' +
+                        '                <span class="date" id="route-date-from-' + i + '"></span>\n' +
+                        '            </div>\n' +
+                        '            <div class="row">\n' +
+                        '                <span class="arrow">&darr;</span>\n' +
+                        '                <span class="price" id="price-' + i + '"> &#8381;</span>\n' +
+                        '                <span class="route-id" id="route-id-' + i + '"></span>\n' +
+                        '                <span class="button"">Выбрать</span>\n' +
+                        '            </div>\n' +
+                        '            <div class="row">\n' +
+                        '                <span class="town" id="route-town-to-' + i + '"></span>\n' +
+                        '                <span class="time" id="route-time-to-' + i + '"></span>\n' +
+                        '                <span class="date" id="route-date-to-' + i + '"></span>\n' +
+                        '            </div>\n' +
+                        '        </div>';
+                    document.getElementById('route-town-from-' + i).innerText = oneWayTrips[i].TOWN_FROM;
+                    document.getElementById('route-town-to-' + i).innerText = oneWayTrips[i].TOWN_TO;
+                    document.getElementById('route-time-from-' + i).innerText = oneWayTrips[i].TIME_DEP;
+                    document.getElementById('route-time-to-' + i).innerText = oneWayTrips[i].TIME_ARR;
+                    document.getElementById('route-date-from-' + i).innerText = oneWayTrips[i].DATE_DEP;
+                    document.getElementById('route-date-to-' + i).innerText = oneWayTrips[i].DATE_ARR;
+                    document.getElementById('price-' + i).innerHTML = oneWayTrips[i].PRICE + '&#8381;';
+                    document.getElementById('route-id-' + i).innerText = oneWayTrips[i].ID;
+                }
+            }).catch(function(error) {
+                console.log(error);
+        });
     }
     else {
         // Два билета
