a518f7e800040a43464821a16d8dcee19d9ee40f
Timofey Gribanov <48593815+TimaGribanov@users.noreply.github.com>
2022-03-03 12:09:59 +0300

    Corrected text, changed links, deleted Twitter, added Finnish

--- script.js
+++ script.js
@@ -10,5 +10,59 @@ window.onload = function calcAge() {
     } else {
         age = ageY - 1;
     }
-    document.getElementById('age').innerText = age;
+    document.getElementById('ageEn').innerText = age;
+    document.getElementById('ageRu').innerText = age;
+    document.getElementById('ageFi').innerText = age;
 };
+
+const langEn = document.getElementById('lang-en');
+const langRu = document.getElementById('lang-ru');
+const langFi = document.getElementById('lang-fi');
+
+function changeLang(targetLang) {
+    const mainEn = document.getElementById('english');
+    const mainRu = document.getElementById('russian');
+    const mainFi = document.getElementById('finnish');
+
+    const footerEn = document.getElementById('footer-en');
+    const footerRu = document.getElementById('footer-ru');
+
+    if (targetLang === 'en') {
+        mainRu.style.display = 'none';
+        mainEn.style.display = 'block';
+        mainFi.style.display = 'none';
+
+        footerRu.style.display = 'none';
+        footerEn.style.display = 'block';
+
+        langRu.style.textDecoration = 'none';
+        langEn.style.textDecoration = 'underline';
+        langFi.style.textDecoration = 'none';
+    } else if (targetLang === 'ru') {
+        mainRu.style.display = 'block';
+        mainEn.style.display = 'none';
+        mainFi.style.display = 'none';
+
+        footerRu.style.display = 'block';
+        footerEn.style.display = 'none';
+
+        langRu.style.textDecoration = 'underline';
+        langEn.style.textDecoration = 'none';
+        langFi.style.textDecoration = 'none';
+    } else {
+        mainRu.style.display = 'none';
+        mainEn.style.display = 'none';
+        mainFi.style.display = 'block';
+
+        footerRu.style.display = 'none';
+        footerEn.style.display = 'block';
+
+        langRu.style.textDecoration = 'none';
+        langEn.style.textDecoration = 'none';
+        langFi.style.textDecoration = 'underline';
+    }
+}
+
+langEn.onclick = function() {changeLang('en')}
+langRu.onclick = function() {changeLang('ru')}
+langFi.onclick = function() {changeLang('fi')}
