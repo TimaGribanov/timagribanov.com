e39889acb791e9b38c86e24d373025f4e20ad106
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2025-04-02 17:13:56 +0200

    added generateStaticParams

--- src/app/[lang]/page.jsx
+++ src/app/[lang]/page.jsx
@@ -7,6 +7,14 @@ import Header from '@/components/Header'
 import Line from '@/components/Line'
 import Translation from '@/components/Translation'
 
+export async function generateStaticParams() {
+  const routes = await fetch('https://timagribanov.com/').then((res) => res.json())
+
+  return routes.map((route) => ({
+    lang: route.lang,
+  }))
+}
+
 export default async function Home({params}) {
   const {lang} = await params
   const dict = await getDictionary(lang)
