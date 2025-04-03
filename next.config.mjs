9f4aa21e5bcc4cf99476529559aeb6f2bd7d21a2
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2025-04-03 11:19:38 +0200

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'build',
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
