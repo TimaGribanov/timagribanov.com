54921b72d1e4e9fb0b077acb3349d53516b5522f
Tima Gribanov <48593815+TimaGribanov@users.noreply.github.com>
2025-06-26 17:02:38 +0200

    Update Links.jsx

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -9,7 +9,7 @@ const Links = () => {
         <h1>{t('links')}</h1>
         <p>LinkedIn: <a href='https://www.linkedin.com/in/timagribanov/' target='_blank'>in/timagribanov</a></p>
         <p>Github: <a href='https://github.com/TimaGribanov' target='_blank'>TimaGribanov</a></p>
-        <p><a href='https://timagribanov.com/cv/' target='_blank'>{t('cv')}</a></p>
+        <p><a href='https://timagribanov.com/cv' target='_blank'>{t('cv')}</a></p>
       </div>
     </section>
   )
